c472c625d79fa5f3045a3ac192246b304b216fbb
kp67 <keerthanap773@gmail.com>
2020-07-10 12:18:52 +0530

    From APJ region guy

---
+++ src/Changes/Pract.java
@@ -0,0 +1,12 @@
+package Changes;
+
+public class Pract {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Grid Practise");
+		System.out.println("Push");
+		System.out.println("Pull");
+	}
+
+}
